e452512023ee035bc49d3c5572c878aeb8024c19
jlefevre <jlefevre@soe.ucsc.edu>
2018-01-10 17:02:25 -0800

    added ceph -DCEPH_LIBRARIES flag.

--- scripts/jeff/runme.sh
+++ scripts/jeff/runme.sh
@@ -2,10 +2,11 @@
 echo "a short script to cmake, make, ctest adios2 jeff dev files."
 echo "..."
 sleep 1
+#/usr/lib/x86_64-linux-gnu/librados.so
 if ! [ -z "$1" ]; then
 	if [ $1 = "clean" ]; then 
 		make clean;
-		cmake -DCMAKE_INSTALL_PREFIX=/usr/local/adios2 -DADIOS1_DIR=/usr/local/adios1/ -DADIOS1_USE_STATIC_LIBS=ON -DADIOS2_USE_ADIOS1=ON -DADIOS1_LIBRARY=/usr/local/adios1/lib/libadios.a -DADIOS1_LIBRARY_PATH=/usr/local/adios1/lib/libadios.a -DADIOS1_INCLUDE_DIR=/usr/local/adios1/include -DADIOS2_USE_CEPH=ON -DCEPH_INCLUDE_DIRS=/usr/include/rados ../source;
+		cmake -DCMAKE_INSTALL_PREFIX=/usr/local/adios2 -DADIOS1_DIR=/usr/local/adios1/ -DADIOS1_USE_STATIC_LIBS=ON -DADIOS2_USE_ADIOS1=ON -DADIOS1_LIBRARY=/usr/local/adios1/lib/libadios.a -DADIOS1_LIBRARY_PATH=/usr/local/adios1/lib/libadios.a -DADIOS1_INCLUDE_DIR=/usr/local/adios1/include -DADIOS2_USE_CEPH=ON -DCEPH_INCLUDE_DIRS=/usr/include/rados -DCEPH_LIBRARIES=/usr/lib/x86_64-linux-gnu/librados.so ../source;
 	else 
 		echo "skipping make clean & cmake"; 
 	fi
